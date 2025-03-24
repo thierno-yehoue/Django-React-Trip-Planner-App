119ae0690d71715db7060e451e5eda016652efcc
Sedo Thierno <thiernoyehoue@gmail.com>
2025-03-24 11:39:55 +0000

    whitnoise commit

--- trip_app_server/backend/settings.py
+++ trip_app_server/backend/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -39,11 +40,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = ['https://tripplannerapp.vercel.app/'
-                          ]
+CORS_ALLOW_ALL_ORIGINS = ['tripplannerapp.vercel.app/']
 
 ROOT_URLCONF = 'backend.urls'
 
